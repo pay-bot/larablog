916eab984d67f66face76762606ca570aae0e8fe
unknown <afahri22@yahoo.com>
2021-07-21 20:30:39 +0700

    m

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="{{ route('create') }}" class="dropdown-item">Create post</a>
+                                    <a href="{{ route('post.create') }}" class="dropdown-item">Create post</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
